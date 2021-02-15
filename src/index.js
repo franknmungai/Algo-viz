934944be01e794b41d7c40e30412d8784be5fff9
unknown <frankmnganga@gmail.com>
2021-02-15 22:52:58 +0300

    render tiles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SimpleTiles from './algorithms/matrices/simple-tiles';
 import BubbleSort from './algorithms/sorting/bubble-sort';
 import HappySort from './algorithms/sorting/happy-sort';
 import MergeSort from './algorithms/sorting/merge-sort';
@@ -8,10 +9,11 @@ import App from './App.js';
 
 ReactDOM.render(
 	<React.Fragment>
-		<BubbleSort />
+		{/* <BubbleSort />
 		<HappySort />
 		<SelectionSort />
-		<MergeSort />
+		<MergeSort /> */}
+		<SimpleTiles />
 	</React.Fragment>,
 	document.getElementById('root')
 );
