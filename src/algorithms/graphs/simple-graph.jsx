083e92b01e5939f00287f5e0f15f2c5e43b5f554
unknown <frankmnganga@gmail.com>
2021-02-19 22:50:12 +0300

    random nodes

--- src/algorithms/graphs/simple-graph.jsx
+++ src/algorithms/graphs/simple-graph.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createGraph, createNodes } from './utils';
 import './styles.css';
 import { delay } from '../../utils/async-delay';
@@ -6,11 +6,20 @@ import { delay } from '../../utils/async-delay';
 const graph = createGraph(500);
 
 const Graph = () => {
-	const [nodes] = useState(() => createNodes(graph));
+	const [nodes, setNodes] = useState(() => createNodes(graph));
 	const [visited, setVisited] = useState([]);
-	const startNode = useRef();
+	const [startNode, setStartNode] = useState(0);
 
-	const bfs = async (node = 0) => {
+	useEffect(() => console.log(startNode), [startNode]);
+	useEffect(() => {
+		const newNodes = [...nodes];
+		newNodes.sort(() => Math.random() - 0.5);
+		setNodes([...newNodes]);
+		// eslint-disable-next-line
+	}, []);
+
+	const bfs = async (node) => {
+		console.log({ node });
 		const visited = [node];
 		setVisited((nodes) => nodes.concat(node));
 		const queue = [node];
@@ -20,9 +29,9 @@ const Graph = () => {
 			await delay(7);
 			for (const neighbor of graph[vertex]) {
 				if (!visited.includes(neighbor)) {
+					queue.push(neighbor);
 					visited.push(neighbor);
 					setVisited((nodes) => [...nodes, neighbor]);
-					queue.push(neighbor);
 					await delay(7);
 				}
 			}
@@ -35,37 +44,47 @@ const Graph = () => {
 
 		while (stack.length) {
 			const node = stack.pop();
-			visited.push(node);
 			setVisited((nodes) => nodes.concat(node));
 			await delay(7);
 			for (const neighbor of graph[node]) {
 				if (!visited.includes(neighbor)) {
 					setVisited((nodes) => [...nodes, neighbor]);
 					stack.push(neighbor);
+					visited.push(node);
 					await delay(7);
 				}
 			}
+		}
+	};
+
+	const rewind = async () => {
+		for (let i = 0; i < visited.length; i++) {
+			await delay(7);
+			setVisited((nodes) => nodes.filter((node) => node !== visited[i]));
 		}
 	};
 	return (
 		<div className="root">
 			<h1 className="title">Graphs</h1>
 			<div className="controls">
-				<button onClick={bfs.bind(this, startNode.current)}>
-					Breadth First
-				</button>
-				<button className="dfs" onClick={dfs.bind(this, startNode.current)}>
+				<button onClick={bfs.bind(this, startNode)}>Breadth First</button>
+				<button className="dfs" onClick={dfs.bind(this, startNode)}>
 					Depth First
 				</button>
+				<button className="dfs" onClick={rewind}>
+					Rewind
+				</button>
+				<button onClick={() => setVisited([])}>Refresh</button>
 			</div>
 			<div className="graph">
 				{nodes.map((node) => (
 					<Vertex
 						key={node}
 						onClick={() => {
-							startNode.current = node;
+							setStartNode(node);
 							setVisited((nodes) => nodes.concat(node));
 						}}
+						visited={visited.includes(node)}
 					/>
 				))}
 			</div>
@@ -74,6 +93,6 @@ const Graph = () => {
 };
 
 const Vertex = ({ visited, onClick }) => {
-	return <div className={`vertex ${visited && 'visited'}`} onclick={onClick} />;
+	return <div className={`vertex ${visited && 'visited'}`} onClick={onClick} />;
 };
 export default Graph;
