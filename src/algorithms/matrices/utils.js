b4ce36c9d3791e6011d45dffafe4f254e2bc589f
unknown <frankmnganga@gmail.com>
2021-02-19 15:13:34 +0300

    createMatrix and randInt

--- src/algorithms/matrices/utils.js
+++ src/algorithms/matrices/utils.js
@@ -1,5 +1,23 @@
+/**
+ * Creates a matrix m x n
+ * @param {number} m columns
+ * @param {number} n rows
+ * @returns {number[][]}
+ */
 export const createMatrix = (m = 10, n = 30) => {
 	return Array.from({ length: m }, (_, i) =>
 		Array.from({ length: n }, (_, j) => `${i}${j}`)
 	);
 };
+
+/**
+ * Generates a random number between a and b
+ * @param {number} a lower limit value
+ * @param {number} b upper limit, non-inclusive
+ * @returns {number}
+ */
+export const randInt = (a, b) => {
+	return Math.floor(Math.random() * b - a);
+};
+
+randInt(2, 7); // 5
